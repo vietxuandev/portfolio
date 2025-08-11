74c5362c121acd0523f47e2a09d1e4ef356ded28
Jayden Nguyen <xuan.nguyen.tpv@one-line.com>
2025-08-12 02:00:56 +0700

    fix error

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,6 +3,7 @@ import { Card as MuiCard, CardProps } from "@mui/material";
 export function Card(props: CardProps) {
   return (
     <MuiCard
+      elevation={0}
       sx={{ flex: 1, aspectRatio: 46 / 35, position: "relative" }}
       {...props}
     />
